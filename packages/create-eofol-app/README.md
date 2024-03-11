689011dd287abecbc7fe5068a8dc97f4f5e970b6
wurducius <wurducius@gmail.com>
2024-03-11 01:03:57 +0100

    Add create-eofol-app script, bump to version 0.2.5

---
+++ packages/create-eofol-app/README.md
@@ -0,0 +1,11 @@
+# crete-eofol-app
+
+Create a reactive app from template with zero configuration!
+
+## Usage
+
+npx @eofol/create-eofol-app <project-name>
+
+## Author
+
+Jakub Eliáš wurducius@gmail.com
---
+++ packages/create-eofol-app/bin/create-eofol-app.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+const { spawn, primary, error } = require("@eofol/eofol-dev-utils");
+
+console.log(primary("Create eofol app"));
+
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
+
+const args = process.argv.slice(2);
+
+if (args.length < 1 || !args[0]) {
+  console.log(
+    error(
+      "Please provide a project name as an argument like `npx create-eofol-app project-name`."
+    )
+  );
+  process.exit(1);
+}
+
+const projectName = args[0];
+const projectPath = path.resolve(process.cwd(), projectName);
+
+if (fs.existsSync(projectPath)) {
+  console.log(
+    error(
+      `Project directory ${projectName} already exists. Cannot create eofol app at this path.`
+    )
+  );
+  process.exit(1);
+}
+
+console.log(primary(`Setting up project ${projectName} at ${projectPath}`));
+
+// create project directory
+fs.mkdirSync(projectPath);
+process.chdir(projectName);
+
+// sparse checkout eofol-app package from github repository to project directory
+spawn.sync("git", [
+  "clone",
+  "--filter=blob:none",
+  "--no-checkout",
+  "--depth",
+  "1",
+  "--sparse",
+  "https://github.com/wurducius/eofol",
+  ".",
+]);
+spawn.sync("git", ["sparse-checkout", "add", "packages/eofol-app"]);
+spawn.sync("git", ["checkout"]);
+
+// install
+process.chdir("packages/eofol-app");
+spawn.sync("npm", ["install"]);
+
+console.log(primary(`Your project is ready at ${projectPath}`));
