01ce0cd4ef1b240bf00d94195f7959dad16e0cd4
wurducius <wurducius@gmail.com>
2024-03-13 21:24:27 +0100

    Type CSS properties object, some extractions

--- packages/eofol/src/styles/sx.ts
+++ packages/eofol/src/styles/sx.ts
@@ -1,55 +1,6 @@
 import { customElementRegistry } from "../core/registry";
-
-// source: https://github.com/garycourt/murmurhash-js/blob/master/murmurhash2_gc.js
-function murmurhash2_32_gc(str: string, seed: number) {
-  var l = str.length,
-    h = seed ^ l,
-    i = 0,
-    k;
-
-  while (l >= 4) {
-    k =
-      (str.charCodeAt(i) & 0xff) |
-      ((str.charCodeAt(++i) & 0xff) << 8) |
-      ((str.charCodeAt(++i) & 0xff) << 16) |
-      ((str.charCodeAt(++i) & 0xff) << 24);
-
-    k =
-      (k & 0xffff) * 0x5bd1e995 + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16);
-    k ^= k >>> 24;
-    k =
-      (k & 0xffff) * 0x5bd1e995 + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16);
-
-    h =
-      ((h & 0xffff) * 0x5bd1e995 +
-        ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)) ^
-      k;
-
-    l -= 4;
-    ++i;
-  }
-
-  switch (l) {
-    case 3:
-      h ^= (str.charCodeAt(i + 2) & 0xff) << 16;
-    case 2:
-      h ^= (str.charCodeAt(i + 1) & 0xff) << 8;
-    case 1:
-      h ^= str.charCodeAt(i) & 0xff;
-      h =
-        (h & 0xffff) * 0x5bd1e995 +
-        ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16);
-  }
-
-  h ^= h >>> 13;
-  h = (h & 0xffff) * 0x5bd1e995 + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16);
-  h ^= h >>> 15;
-
-  return h >>> 0;
-}
-
-// @TODO typing
-type CSSObject = Record<string, string>;
+import { CSSObject } from "../types/common-types";
+import { murmurhash2_32_gc } from "../util/util";
 
 const seed = new Date().getMilliseconds();
 
@@ -63,8 +14,9 @@ const objectNotationToCSSNotation = (label: string) =>
     )
     .join("");
 
-function sx(style: CSSObject): string {
+function sx(style: CSSObject, excludeCustomElements: boolean): string {
   const hash = "e" + murmurhash2_32_gc(JSON.stringify(style), seed).toString();
+
   if (!styleCache.includes(hash)) {
     // @ts-ignore
     const cssStyle = Object.keys(style).reduce(
@@ -73,20 +25,25 @@ function sx(style: CSSObject): string {
         " " +
         objectNotationToCSSNotation(next) +
         ": " +
+        // @ts-ignore
         objectNotationToCSSNotation(style[next]) +
         ";",
       ""
     );
+
     const last = document.styleSheets[document.styleSheets.length - 1];
     const rule = `.${hash} { ${cssStyle} }`;
     last.insertRule(rule);
-    Object.keys(customElementRegistry).forEach((id) => {
-      const instance = customElementRegistry[id];
-      const styleElement = instance.root?.querySelector("style");
-      if (styleElement) {
-        styleElement.innerHTML += " " + rule;
-      }
-    });
+
+    if (!excludeCustomElements) {
+      Object.keys(customElementRegistry).forEach((id) => {
+        const instance = customElementRegistry[id];
+        const styleElement = instance.root?.querySelector("style");
+        if (styleElement) {
+          styleElement.innerHTML += " " + rule;
+        }
+      });
+    }
   }
   return hash;
 }
--- packages/eofol/src/types/common-types.ts
+++ packages/eofol/src/types/common-types.ts
@@ -1,5 +1,9 @@
+import type * as CSS from "csstype";
+
 export type Multi<T> = T | T[];
 
 export type Optional<T> = T | undefined;
 
 export type MultiOptional<T> = Optional<Multi<T>>;
+
+export type CSSObject = CSS.Properties;
